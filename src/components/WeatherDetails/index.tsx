c5ac0729754174e2f82b9c29bd00650c00ddbcc6
Renan <renan_gian@hotmail.com>
2023-09-25 19:35:37 -0300

    chore: lint adjust

--- src/components/WeatherDetails/index.tsx
+++ src/components/WeatherDetails/index.tsx
@@ -1,14 +1,14 @@
-import { View } from 'react-native';
+import { View } from "react-native";
 
-import sun from '@assets/sun.svg';
-import rain from '@assets/rain.svg';
-import wind from '@assets/wind.svg';
-import drop from '@assets/drop.svg';
-import thermometer from '@assets/thermometer.svg';
+import sun from "@assets/sun.svg";
+import rain from "@assets/rain.svg";
+import wind from "@assets/wind.svg";
+import drop from "@assets/drop.svg";
+import thermometer from "@assets/thermometer.svg";
 
-import { WeatherItem } from '@components/WeatherItem';
+import { WeatherItem } from "@components/WeatherItem";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
 export interface WeatherDetailsProps {
   temp_kf: string;
@@ -43,18 +43,9 @@ export function WeatherDetails({ data }: Props) {
         value={data.wind_speed}
       />
 
-      <WeatherItem
-        icon={drop}
-        title="Umidade do ar"
-        value={data.humidity}
-      />
+      <WeatherItem icon={drop} title="Umidade do ar" value={data.humidity} />
 
-      <WeatherItem
-        icon={sun}
-        title="Índice UV"
-        value={data.temp_kf}
-        isLast
-      />
+      <WeatherItem icon={sun} title="Índice UV" value={data.temp_kf} isLast />
     </View>
   );
 }
